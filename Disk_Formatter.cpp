7a3c098c1aac75c9b452a206e96efa94235b1707
MERT <kabukcuoglumert@gmail.com>
2024-01-23 22:16:21 +0300

    Initial Commit - Lists all Removable Disks and shows volume information

---
+++ Disk_Formatter.cpp
@@ -0,0 +1,46 @@
+
+#include <Windows.h>
+#include <iostream>
+#include <tchar.h>
+
+int main()
+{
+    _TCHAR buffer[MAX_PATH];
+    DWORD drives = GetLogicalDriveStrings(MAX_PATH, buffer);
+
+    if (drives == 0) 
+    {
+        std::cerr << "Error getting logical drives. Error code: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    // Iterate through each drive
+    for (DWORD i = 0; i < drives; i += 4) 
+    {
+        _TCHAR drivePath[4] = { buffer[i], buffer[i + 1], buffer[i + 2], buffer[i + 3] };
+
+        // Check if the drive is a removable drive (e.g., SD card)
+        UINT driveType = GetDriveType(drivePath);
+
+        if (driveType == DRIVE_REMOVABLE) 
+        {
+            std::wcout << "Removable drive found: " << drivePath <<'\n';
+
+            // Get volume information
+            _TCHAR volumeName[MAX_PATH];
+            _TCHAR fileSystem[MAX_PATH];
+            DWORD serialNumber;
+            DWORD maxComponentLength;
+            DWORD fileSystemFlags;
+
+            if (GetVolumeInformation(drivePath, volumeName, MAX_PATH, &serialNumber, &maxComponentLength, &fileSystemFlags, fileSystem, MAX_PATH)) 
+            {
+                std::wcout << "Volume Name: " << volumeName << '\n';;
+                std::wcout << "Serial Number: " << serialNumber << '\n';
+                std::wcout << "File System: " << fileSystem << std::endl;
+            }
+            else 
+                std::cerr << "Error getting volume information. Error code: " << GetLastError() << std::endl;
+        }
+    }
+}
